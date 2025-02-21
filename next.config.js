ee95c8cb083b206618ff62bc42baac19142b1460
jusbuckingham <jus.buckingham@gmail.com>
2025-02-21 11:28:25 -0800

    Remove next.config.js to use next.config.cjs

--- next.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  reactStrictMode: true,
-  env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
-    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
-    WEB3_PROVIDER_URL: process.env.WEB3_PROVIDER_URL,
-  },
-}
